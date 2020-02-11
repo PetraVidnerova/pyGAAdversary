326a34059f8eaf4fb49ff1e187c44bd509f8dd73
Petra Vidnerová <petra.vidnerova@gmail.com>
2020-02-11 22:18:47 +0100

    for over ID

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ function wait {
     while $WAIT 
     do 
 	PS=`ps x | grep "python main.py" | wc --lines`
-	if test $PS -lt 30
+	if test $PS -lt 28
 	then
 	    WAIT=false 
 	else 
@@ -22,128 +22,14 @@ function run {
 }
 
 
-# run CNN 1 0 10.0 
-# run CNN 1 0 5.0 
-# run CNN 1 0 3.0 
-# run CNN 1 0 1.0 
 
-# run CNN 1 0 0 
-# run CNN 1 2 0
-# run CNN 1 3 0
-# run CNN 1 4 0
-# run CNN 1 5 0 
-# run CNN 1 6 0
-# run CNN 1 7 0
-# run CNN 1 8 0
-# run CNN 1 9 0
+for ID in `seq 3 9`; do
+    for TARGET in `seq 0 9`; do
+	for IMAGE in `seq 0 9`; do
+	    if test $TARGET -ne $IMAGE; then
+		run CNN $TARGET $IMAGE $ID
+	    fi
+	done
+    done 
+done
 
-# run CNN 0 1 0 
-# run CNN 0 2 0
-# run CNN 0 3 0
-# run CNN 0 4 0
-# run CNN 0 5 0 
-# run CNN 0 6 0
-# run CNN 0 7 0
-# run CNN 0 8 0
-# run CNN 0 9 0
-
-# run CNN 2 0 0 
-# run CNN 2 1 0
-# run CNN 2 3 0
-# run CNN 2 4 0
-# run CNN 2 5 0 
-# run CNN 2 6 0
-# run CNN 2 7 0
-# run CNN 2 8 0
-# run CNN 2 9 0
-
-# run CNN 3 0 0 
-# run CNN 3 1 0
-# run CNN 3 2 0
-# run CNN 3 4 0
-# run CNN 3 5 0 
-# run CNN 3 6 0
-# run CNN 3 7 0
-# run CNN 3 8 0
-# run CNN 3 9 0
-
-# run CNN 4 0 0 
-# run CNN 4 1 0
-# run CNN 4 2 0
-# run CNN 4 3 0
-# run CNN 4 5 0 
-# run CNN 4 6 0
-# run CNN 4 7 0
-# run CNN 4 8 0
-# run CNN 4 9 0
-
-for TARGET in `seq 7 9`; do
-    for IMAGE in `seq 0 9`; do
-	if test $TARGET -ne $IMAGE; then
-	    run CNN $TARGET $IMAGE 0
-	fi
-    done
-done 
-
-
-# run CNN 1 2 1.0 
-# run CNN 1 2 2.0 
-# run CNN 1 2 3.0 
-# run CNN 1 2 4.0
-# run CNN 1 2 5.0  
-# run CNN 1 2 6.0 
-# run CNN 1 2 7.0 
-# run CNN 1 2 8.0 
-# run CNN 1 2 9.0 
-# run CNN 1 2 10.0 
-
-# run CNN 1 3 1.0 
-# run CNN 1 3 2.0 
-# run CNN 1 3 3.0 
-# run CNN 1 3 4.0
-# run CNN 1 3 5.0  
-# run CNN 1 3 6.0 
-# run CNN 1 3 7.0 
-# run CNN 1 3 8.0 
-# run CNN 1 3 9.0 
-# run CNN 1 3 10.0 
-
-
-# run CNN 1 2 1.0 
-# run CNN 1 3 1.0 
-# run CNN 1 4 1.0 
-# run CNN 1 5 1.0 
-# run CNN 1 6 1.0 
-# run CNN 1 7 1.0 
-# run CNN 1 8 1.0 
-# run CNN 1 9 1.0 
-
-# run CNN 0 1 1.0
-# run CNN 0 2 1.0
-# run CNN 0 3 1.0
-# run CNN 0 4 1.0
-# run CNN 0 5 1.0
-# run CNN 0 6 1.0
-# run CNN 0 7 1.0
-# run CNN 0 8 1.0
-# run CNN 0 9 1.0
-
-# run CNN 2 0 1.0
-# run CNN 2 1 1.0
-# run CNN 2 3 1.0
-# run CNN 2 4 1.0
-# run CNN 2 5 1.0
-# run CNN 2 6 1.0
-# run CNN 2 7 1.0
-# run CNN 2 8 1.0
-# run CNN 2 9 1.0
-
-# run SVM_poly 2 0 1.0
-# run SVM_poly 2 1 1.0
-# run SVM_poly 2 3 1.0
-# run SVM_poly 2 4 1.0
-# run SVM_poly 2 5 1.0
-# run SVM_poly 2 6 1.0
-# run SVM_poly 2 7 1.0
-# run SVM_poly 2 8 1.0
-# run SVM_poly 2 9 1.0
