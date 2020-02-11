0ac1c4c25c77578b433294fe2aa4de23029781df
Petra Vidnerova <petra@hakld.adui.cs.cas.cz>
2020-02-11 21:46:49 +0100

    use for cycle

--- run_roman.sh
+++ run_roman.sh
@@ -2,39 +2,17 @@
 
 
 function run {
-#    wait
-    python main.py $1 $2 $3 $4 > $1_$2_$3_$4.log 
-#    sleep 5s
+   python main.py $1 $2 $3 $4 > $1_$2_$3_$4.log 
 }
 
-run CNN 1 0 1 
-run CNN 1 2 1
-run CNN 1 3 1
-run CNN 1 4 1
-run CNN 1 5 1 
-run CNN 1 6 1
-run CNN 1 7 1
-run CNN 1 8 1
-run CNN 1 9 1
+$ID=2
 
-run CNN 0 1 1 
-run CNN 0 2 1
-run CNN 0 3 1
-run CNN 0 4 1
-run CNN 0 5 1 
-run CNN 0 6 1
-run CNN 0 7 1
-run CNN 0 8 1
-run CNN 0 9 1
-
-run CNN 2 0 1 
-run CNN 2 1 1
-run CNN 2 3 1
-run CNN 2 4 1
-run CNN 2 5 1 
-run CNN 2 6 1
-run CNN 2 7 1
-run CNN 2 8 1
-run CNN 2 9 1
+for TARGET in `seq 4 9`; do
+    for IMAGE in `seq 0 9`; do
+	if test $TARGET -ne $IMAGE; then
+	    run CNN $TARGET $IMAGE $ID
+	fi
+    done
+done 
 
 
