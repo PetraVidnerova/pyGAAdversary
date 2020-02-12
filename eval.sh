b44e808e3bfbd1f0417243faecb18c16e9127bd1
Petra Vidnerova <petra@hakld.adui.cs.cas.cz>
2020-02-12 10:34:52 +0100

    use params

--- eval.sh
+++ eval.sh
@@ -5,14 +5,14 @@ function run {
     GEN=`tail -n 3 $1_$2_$3_$4.log | head -n 1 | sed 's/\t/;/g' | cut -d';' -f3`
     echo -n $GEN
 }
-
-ID=$1
+MODEL=$1
+ID=$2
 
 
 for TARGET in `seq 0 9`; do
     for IMAGE in `seq 0 9`; do
 	if test $TARGET -ne $IMAGE; then
-	    run CNN $TARGET $IMAGE $ID
+	    run $MODEL $TARGET $IMAGE $ID
 	else 
 	    echo -n 0
 	fi
